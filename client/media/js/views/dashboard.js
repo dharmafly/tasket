503d204523f7128baac8fa60c0ebaacae5a7e0b0
Aron Carroll <self@aroncarroll.com>
2011-03-18 11:33:47 +0000

    Added methods to update the dashboard lists

--- client/media/js/views/dashboard.js
+++ client/media/js/views/dashboard.js
@@ -4,6 +4,34 @@ var Dashboard = View.extend({
     render: function () {
         var rendered = tim('dashboard');
         this.elem.html(rendered);
+
+        // Update each of the task lists.
+        _.each(['MyTasks', 'MyHubs', 'ManagedTasks'], function (method) {
+            this['update' + method]();
+        }, this);
+
+        return this;
+    },
+    updateManagedTasks: function () {
+        this.updateList('.managed-tasks ul', this.model.tasks.owned);
+    },
+    updateMyTasks: function () {
+        this.updateList('.my-tasks ul', this.model.tasks.claimed);
+    },
+    updateMyHubs: function () {
+        this.updateList('.my-projects ul', this.model.hubs.owned);
+    },
+    updateList: function(selector, models){
+        var mapped = models.map(function (model) {
+            return {
+                href: '#/' + model.id,
+                title: model.get('title')
+            };
+        });
+
+        this.$(selector).html(tim('dashboard-link', {
+           links: mapped
+        }));
         return this;
     }
 });
