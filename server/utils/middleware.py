2a79fd19d64e64b653ee4fc90c61ef0831aba7c4
Sym Roe <sym.roe@talusdesign.co.uk>
2011-03-21 11:25:25 +0000

    Fixes #41 refs #39, clean up CORS auth middleware

--- server/utils/middleware.py
+++ server/utils/middleware.py
@@ -46,49 +46,13 @@ class CORSAuthorizationMiddleware():
             engine = import_module(settings.SESSION_ENGINE)
             session_key = request.META.get('HTTP_AUTHORIZATION', None)
             request.session = engine.SessionStore(session_key)
-        else:
-            # Copied from django.contrib.sessions.middleware.SessionMiddleware
-            engine = import_module(settings.SESSION_ENGINE)
-            session_key = request.COOKIES.get(settings.SESSION_COOKIE_NAME, None)
-            request.session = engine.SessionStore(session_key)
         
 
     def process_response(self, request, response):
-        """
-        Taken from django.contrib.sessions.middleware.SessionMiddleware.
-        
-        Do *everything* like normal, but also set the Authorization header to the
-        Session Id.
-        
-        ----
-        If request.session was modified, or if the configuration is to save the
-        session every time, save the changes and set a session cookie.
-        """
         try:
-            accessed = request.session.accessed
-            modified = request.session.modified
+            response['Authorization'] = request.session.session_key
         except AttributeError:
             pass
-        else:
-            if accessed:
-                patch_vary_headers(response, ('Cookie',))
-            if modified or settings.SESSION_SAVE_EVERY_REQUEST:
-                if request.session.get_expire_at_browser_close():
-                    max_age = None
-                    expires = None
-                else:
-                    max_age = request.session.get_expiry_age()
-                    expires_time = time.time() + max_age
-                    expires = cookie_date(expires_time)
-                # Save the session data and refresh the client cookie.
-                request.session.save()
-                response.set_cookie(settings.SESSION_COOKIE_NAME,
-                        request.session.session_key, max_age=max_age,
-                        expires=expires, domain=settings.SESSION_COOKIE_DOMAIN,
-                        path=settings.SESSION_COOKIE_PATH,
-                        secure=settings.SESSION_COOKIE_SECURE or None)
-        
-            response['Authorization'] = request.session.session_key
         
         return response
     
--- web/global_settings.py
+++ web/global_settings.py
@@ -54,7 +54,7 @@ TEMPLATE_LOADERS = (
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
-    # 'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'utils.middleware.CORSAuthorizationMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
