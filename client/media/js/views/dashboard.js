dc4ab8388b2574239b908e9c88262ee5ec8c3120
Aron Carroll <self@aroncarroll.com>
2011-04-05 09:50:38 +0100

    Converting quotes in dashboard.js

--- client/media/js/views/dashboard.js
+++ client/media/js/views/dashboard.js
@@ -1,10 +1,10 @@
 var Dashboard = View.extend({
-    tagName: 'section',
+    tagName: "section",
 
-    className: 'dashboard',
+    className: "dashboard",
 
     classes: {
-        detailShown: 'detail-active'
+        detailShown: "detail-active"
     },
 
     constructor: function Dashboard() {
@@ -15,10 +15,10 @@ var Dashboard = View.extend({
         View.prototype.initialize.apply(this, arguments);
 
         _.bindAll(this,
-            'updateNotifications',
-            'updateUserTasks',
-            'updateUserHubs',
-            'updateManagedTasks'
+            "updateNotifications",
+            "updateUserTasks",
+            "updateUserHubs",
+            "updateManagedTasks"
         );
 
         // Setup the user bindings.
@@ -26,11 +26,11 @@ var Dashboard = View.extend({
 
         // Set up the detail instance.
         this.detail = new DashboardDetail();
-        this.detail.bind('all', _.bind(function (event) {
-            if (event === 'show') {
+        this.detail.bind("all", _.bind(function (event) {
+            if (event === "show") {
                 this.elem.addClass(this.classes.detailShown);
             }
-            else if (event === 'hide') {
+            else if (event === "hide") {
                 this.elem.removeClass(this.classes.detailShown);
             }
         }, this));
@@ -42,11 +42,11 @@ var Dashboard = View.extend({
             methodMap;
 
         methodMap = {
-            'UserHubs':      ['hubs.owned'],
-            'UserTasks':     ['tasks.claimed.claimed'],
-            'ManagedTasks':  ['tasks.owned.done'],
-            'Notifications': [
-              'tasks.owned.claimed', 'tasks.owned.done', 'tasks.claimed.verified'
+            "UserHubs":      ["hubs.owned"],
+            "UserTasks":     ["tasks.claimed.claimed"],
+            "ManagedTasks":  ["tasks.owned.done"],
+            "Notifications": [
+              "tasks.owned.claimed", "tasks.owned.done", "tasks.claimed.verified"
             ]
         };
 
@@ -54,13 +54,13 @@ var Dashboard = View.extend({
         this.model = arguments.length ? user : this.model;
 
         if (this.model) {
-            this.model.bind('change', function (user) {
+            this.model.bind("change", function (user) {
                 _.each(methodMap, function (attributes, method) {
                     // For each of the attributes in the methodMap see if it has
                     // changed if so call the associated method.
                     _.each(attributes, function (attribute) {
                         if (user.hasChanged(attribute)) {
-                            dashboard['update' + method]();
+                            dashboard["update" + method]();
                         }
                     });
                 });
@@ -75,12 +75,12 @@ var Dashboard = View.extend({
     },
 
     render: function () {
-        var rendered = tim('dashboard');
+        var rendered = tim("dashboard");
         this.elem.html(rendered);
 
         // Update each of the task lists.
-        _.each(['Notifications', 'UserTasks', 'UserHubs', 'ManagedTasks'], function (method) {
-            this['update' + method]();
+        _.each(["Notifications", "UserTasks", "UserHubs", "ManagedTasks"], function (method) {
+            this["update" + method]();
         }, this);
 
         this.elem.append(this.detail.hide().render().el);
@@ -91,24 +91,24 @@ var Dashboard = View.extend({
     userStatistics: function(){
         var user = this.model;
         return {
-            ownedClaimed:    user.get('tasks.owned.claimed').length,
-            adminedDone:     user.get('tasks.owned.done').length,      // TODO: if an admin, this should include all done tasks
-            claimedVerified: user.get('tasks.claimed.verified').length // TODO: should be recent verified tasks
+            ownedClaimed:    user.get("tasks.owned.claimed").length,
+            adminedDone:     user.get("tasks.owned.done").length,      // TODO: if an admin, this should include all done tasks
+            claimedVerified: user.get("tasks.claimed.verified").length // TODO: should be recent verified tasks
         };
     },
 
     // Updates the user status box.
     updateNotifications: function () {
-        var stats = this.model && this.model.get('statistics'),
-            notifications = this.$('.notifications');
+        var stats = this.model && this.model.get("statistics"),
+            notifications = this.$(".notifications");
 
         if (stats) {
             notifications.show();
-            notifications.find('a').each(function () {
+            notifications.find("a").each(function () {
                 var anchor = jQuery(this),
                     listItem = anchor.parent(),
-                    type = this.getAttribute('data-type'),
-                    elem = anchor.find('span');
+                    type = this.getAttribute("data-type"),
+                    elem = anchor.find("span");
 
                 if (stats[type]) {
                     elem.text(stats[type]);
@@ -127,49 +127,49 @@ var Dashboard = View.extend({
     updateManagedTasks: function () {
         var tasks = null;
         if (this.model) {
-            tasks = Tasket.getTasks(this.model.get('tasks.owned.done'));
-            tasks.bind('refresh', this.updateManagedTasks);
+            tasks = Tasket.getTasks(this.model.get("tasks.owned.done"));
+            tasks.bind("refresh", this.updateManagedTasks);
         }
-        return this.updateList('.managed-tasks', tasks);
+        return this.updateList(".managed-tasks", tasks);
     },
 
     // Updates the "My Tasks" box.
     updateUserTasks: function () {
         var tasks = null;
         if (this.model) {
-            tasks = Tasket.getTasks(this.model.get('tasks.claimed.claimed'));
-            tasks.bind('refresh', this.updateUserTasks);
+            tasks = Tasket.getTasks(this.model.get("tasks.claimed.claimed"));
+            tasks.bind("refresh", this.updateUserTasks);
         }
-        return this.updateList('.my-tasks', tasks);
+        return this.updateList(".my-tasks", tasks);
     },
 
     // Updates the "My Projects" box.
     updateUserHubs: function () {
         var hubs = null;
         if (this.model) {
-            hubs = Tasket.getHubs(this.model.get('hubs.owned'));
-            hubs.bind('refresh', this.updateUserHubs);
+            hubs = Tasket.getHubs(this.model.get("hubs.owned"));
+            hubs.bind("refresh", this.updateUserHubs);
         }
-        return this.updateList('.my-projects', hubs);
+        return this.updateList(".my-projects", hubs);
     },
 
     // Updates a list of tasks/hubs based on the selector & collection.
     updateList: function(selector, models){
         var mapped;
 
-        if (models && (models.length || models.type === 'hub')) {
+        if (models && (models.length || models.type === "hub")) {
             mapped = models.map(function (model) {
-                var title = model.get('title') || model.get('description');
+                var title = model.get("title") || model.get("description");
                 return {
-                    href:        '#/' + model.type + 's/' + model.id,
+                    href:        "#/" + model.type + "s/" + model.id,
                     title:       truncate(title, 15),
-                    isHub:       model.type === 'hub',
-                    isTask:      model.type === 'task',
-                    showDone:    app.isCurrentUser(model.get('claimedBy')),
-                    showVerify: !app.isCurrentUser(model.get('claimedBy'))
+                    isHub:       model.type === "hub",
+                    isTask:      model.type === "task",
+                    showDone:    app.isCurrentUser(model.get("claimedBy")),
+                    showVerify: !app.isCurrentUser(model.get("claimedBy"))
                 };
             });
-            this.$(selector).show().find('ul').html(tim('dashboard-link', {
+            this.$(selector).show().find("ul").html(tim("dashboard-link", {
                 links: mapped
             }));
         } else {
@@ -179,4 +179,3 @@ var Dashboard = View.extend({
         return this;
     }
 });
-
