f793330ad62fba1be4a0356b2f51185cbe3eec05
Aron Carroll <self@aroncarroll.com>
2011-04-07 14:58:33 +0100

    Fixed issue with Cache not returning Mock object when unsupported

--- client/media/js/dependencies/cache.js
+++ client/media/js/dependencies/cache.js
@@ -39,12 +39,13 @@ var Cache = (function(window){
     catch(e){}
     
     if (!localStorage){
-        (function(){
+        return (function(){
             var Mock = function(){},
                 p = Mock.prototype;
                 
             p.set = p.remove = function(){ return this; };
             p.get = p.wrapper = p.time = function(){};
+            p.localStorage = false;
             return Mock;
         }());
     }
