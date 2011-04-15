5547d7f083f3e509600ad2900471caddc9911ad2
Sym Roe <sym.roe@talusdesign.co.uk>
2011-04-15 13:52:05 +0100

    Fixes #117, Enable CSRF

--- web/global_settings.py
+++ web/global_settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'utils.middleware.CORSAuthorizationMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'utils.middleware.CORSMiddleware'
