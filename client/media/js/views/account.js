8f9707fd6eb852578fc442e8bc7755fca7b04936
Premasagar Rose <p@dharmafly.com>
2011-04-16 07:43:02 -0400

    temp workaround for #175 removed

--- client/media/js/views/account.js
+++ client/media/js/views/account.js
@@ -2,12 +2,6 @@ var Account = SignUp.extend({
     render: function () {
         var data = this.model.toJSON();
 
-        // Temporary workaround for the server returning the email address
-        // field as an array which will break tim().
-        if (typeof data.email !== "string") {
-            data.email = "";
-        }
-
         this.elem.html(tim("account", data)).find(".loading").hide();
         return this;
     }
