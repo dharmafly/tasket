d9fed40a2a65fd9fe899a18a97151286d5564607
Premasagar Rose <p@dharmafly.com>
2011-05-06 13:53:43 -0400

    account change form simplified, as an extension of SignUp form

--- client/media/js/views/account.js
+++ client/media/js/views/account.js
@@ -1,15 +1,4 @@
 var Account = SignUp.extend({
-    constructor: function Account() {
-        Form.prototype.constructor.apply(this, arguments);
-        
-        // Remove the password fields from the User model.
-        this.bind("submit", function (user) {
-            _.each(["password", "password-confirm"], function (attribute) {
-                user.unset(attribute, {silent: true});
-            });
-        });
-    },
-    
     render: function () {
         var data = this.model.toJSON();
 
