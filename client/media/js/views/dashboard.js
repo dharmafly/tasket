6a56745438b3dae7250e754e187bdcb418f5a9f1
Aron Carroll <self@aroncarroll.com>
2011-05-11 21:37:33 +0100

    Re-added missing help icons to dashboard. Fixes #243

--- client/media/js/views/dashboard.js
+++ client/media/js/views/dashboard.js
@@ -9,7 +9,9 @@ var Dashboard = View.extend({
 
     events: {
         "click .notifications": "_onNotificationClick",
-        "click section.quicklinks.my-projects ul.listing li": "toggleHub"
+        "click section.quicklinks.my-projects ul.listing li": "toggleHub",
+        "mouseenter .info": "_toggleHelp",
+        "mouseleave .info": "_toggleHelp"
     },
 
     constructor: function Dashboard() {
@@ -96,7 +98,8 @@ var Dashboard = View.extend({
 
         return this;
     },
-    
+
+
     toggleHub: function(event){
         var hubId = app.dashboard.getHubIdFromAnchor(event.target);
         if (hubId === app.selectedHub){
@@ -272,6 +275,10 @@ var Dashboard = View.extend({
             } while (watch.length);
         });
         return collection;
+    },
+
+    _toggleHelp: function (event) {
+        $(event.target).siblings('.help').toggleClass('active');
     },
 
     // Scroll down to the appropriate listing and highlight the activity links.
