4264cbe435c25e063e0e01dc1f2d9fb7f08830b3
Sym Roe <sym.roe@talusdesign.co.uk>
2011-07-26 22:49:00 +0100

    Properly implement archived filter (true now only shows archived hubs)

--- server/tasks/views.py
+++ server/tasks/views.py
@@ -69,9 +69,11 @@ class HubView(PutView):
             ids = request.GET['ids']
             ids = [i.strip() for i in ids.split(',') if i]
             hubs = hubs.filter(pk__in=ids)
-        if not request.GET.get('archived', False) == 'true':
+        if request.GET.get('archived', False) == 'true':
+          hubs = hubs.exclude(archived_by=None)
+        else:
           hubs = hubs.filter(archived_by=None)
-        
+          
         res = self.res
         res.write(hubs.as_json(request_user=request.user))
         return res
